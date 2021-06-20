51b4e7b23326c6fc5572544a6cefaeeb40cf11cc
ErviaLindasari <ervialindasari1@gmail.com>
2021-06-20 14:53:12 +0700

    Kelas Karyawan buat 3 constructor

--- src/model/Karyawan.java
+++ src/model/Karyawan.java
@@ -29,18 +29,19 @@ import java.time.*;
 
         // constructor (2)
         public Karyawan() {
-            System.out.println("      Data Gaji Pegawai        ");
-            System.out.println("===============================");
+            System.out.println("      Data Gaji Karyawan       ");
+            System.out.println("*******************************");
         }
+
         // constructor (3)
         public Karyawan(String User, String Password)
         {
             this.User = User;
             this.Password = Password;
-            System.out.println("===============================");
+            System.out.println("*******************************");
             System.out.println("Username: "+this.User);
             System.out.println("Password: "+this.Password);
-            System.out.println("===============================");
+            System.out.println("*******************************");
         }
 
         // method menghitung usia Karyawan (1)
