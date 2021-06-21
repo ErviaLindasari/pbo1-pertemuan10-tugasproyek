c960b77814bd83a73ffd134ff48e97442933e4c8
ErviaLindasari <ervialindasari1@gmail.com>
2021-06-21 10:01:06 +0700

    Kelas Main buat 2 objek di tiap kelasnya

--- src/model/Main.java
+++ src/model/Main.java
@@ -5,41 +5,57 @@ import java.time.*;
 public class Main {
     public static void main(String[] args) {
         // contoh objek data Karyawan constructor 1
-        Karyawan k2     = new Karyawan();
+        Karyawan k2     = new Karyawan("Ervia Lindasari");
 
         // contoh objek data Karyawan constructor 2
-        Karyawan k3     = new Karyawan("Ervia Lindasari", "Ervia1234");
+        Karyawan k3 = new Karyawan(123456);
 
         // contoh objek data Karyawan constructor 3
-        Karyawan k1     = new Karyawan("K01",
-                                      "Ervia Lindasari",
-                                       LocalDate.of(1998, 3, 22),
-                                      "Guntung Manggis",
-                                      "089688129532",
-                                      "Belum Menikah",
-                                      1,
-                                      2);
+        Karyawan k1     = new Karyawan();
+        k1.setIdKaryawan("K01");
+        k1.setNama("Ervia Lindasari");
+        k1.setTanggalLahir(LocalDate.of(1998, 3, 22));
+        k1.setAlamat("Guntung Manggis");
+        k1.setNoTelp("089688129532");
+        k1.setStatus("Belum Menikah");
+        k1.setGol(1);
+        k1.setLamaKerja(2);
+
         k1.printKaryawan();
 
         // contoh objek data Karyawan constructor 4
-        Karyawan k5     = new Karyawan();
+        Karyawan k5     = new Karyawan("Vika Permatasari");
 
         // contoh objek data Karyawan constructor 5
-        Karyawan k6     = new Karyawan("Vika Permatasari", "Vika0111");
+        Karyawan k6     = new Karyawan(123678);
 
         // contoh objek data Karyawan constructor 6
-        Karyawan k4     = new Karyawan("K02",
-                                       "Vika Permatasari",
-                                        LocalDate.of(1992, 7, 25),
-                                       "Guntung Payung",
-                                       "08125130006",
-                                       "Menikah",
-                                       2,
-                                       4);
+        Karyawan k4     = new Karyawan();
+        k4.setIdKaryawan("K02");
+        k4.setNama("Ervinadya Aulina Dewi");
+        k4.setTanggalLahir(LocalDate.of(1996, 9, 13));
+        k4.setAlamat("Guntung Payung");
+        k4.setNoTelp("08125130006");
+        k4.setStatus("Menikah");
+        k4.setGol(2);
+        k4.setLamaKerja(4);
+
         k4.printKaryawan();
 
         // contoh objek data karyawan guru
-        Guru k7 = new Guru("K03", "Ervinadya Aulina Dewi", LocalDate.of(1996, 9, 13), "Guntung Payung", "08125130006", "Menikah", 2, 4, "15112208", "Matematika", "MIPA");
-        k7.printKaryawan();
+        Guru g1         = new Guru();
+        g1.setIdKaryawan("K03");
+        g1.setNip("15112208");
+        g1.setNama("Vika Permatasari");
+        g1.setTanggalLahir(LocalDate.of(1994, 7, 22));
+        g1.setAlamat("Guntung Payung");
+        g1.setNoTelp("08125130006");
+        g1.setStatus("Menikah");
+        g1.setGol(3);
+        g1.setLamaKerja(5);
+        g1.setJurusan("Matematika");
+        g1.setBidangKeahlian("MIPA");
+
+        g1.printKaryawan();
     }
 }
